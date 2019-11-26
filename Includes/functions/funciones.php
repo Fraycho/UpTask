e629229a26573b4fdb3012f71365d52ae0e06174
Fray Gómez Marin <Fraycho@users.noreply.github.com>
2019-11-26 17:42:31 -0500

    4

--- Includes/functions/funciones.php
+++ Includes/functions/funciones.php
@@ -1,7 +1,22 @@
 <?php
 
+// Direccion pagina actual
 function paginaActual(){
     $archivo = basename($_SERVER['PHP_SELF']);
     $pagina = str_replace('.php', '', $archivo);
     return $pagina;
+}
+
+// Consultas
+
+    // Obtener todos los proyectos
+function obtenerProyectos(){
+    include 'conexion.php';
+
+    try {
+        return $conexion->query('SELECT id, nombre FROM proyectos');       
+    } catch (Exception $e) {
+        echo "Error!: " . $e->getMessage();
+        return false;
+    }
 }
