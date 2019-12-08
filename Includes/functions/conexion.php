648635acc83801996f79f852d0f5d491db837623
Fraycho <fraycho_@hotmail.com>
2019-12-07 22:17:33 -0500

    4

--- Includes/functions/conexion.php
+++ Includes/functions/conexion.php
@@ -1,9 +1,9 @@
 <?php
 
-    define('DB_HOST', 'localhost');
-    define('DB_USUARIO', 'root');
-    define('DB_CONTRASEÑA', '');
-    define('DB_NOMBRE', 'uptask');
+    if (!defineD('DB_HOST')) define('DB_HOST', 'localhost');
+    if (!defined('DB_USUARIO')) define('DB_USUARIO', 'root');
+    if (!defined('DB_CONTRASEÑA')) define('DB_CONTRASEÑA', '');;
+    if (!defined('DB_NOMBRE')) define('DB_NOMBRE', 'uptask');;
 
     $conexion = new mysqli(DB_HOST, DB_USUARIO, DB_CONTRASEÑA, DB_NOMBRE);
 
--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php
+    error_reporting(E_ALL ^ E_NOTICE);
     include "includes/functions/sesiones.php";
     include "includes/functions/funciones.php";
     include "includes/templates/header.php";
@@ -15,35 +16,42 @@
     <?php include "includes/templates/sidebar.php"; ?>
 
     <main class="contenido-principal">
-        <h1>Proyecto Actual: 
-            <?php $proyecto = obtenerNombreProyecto($id_proyecto);
-            
-            foreach($proyecto as $nombre): ?>
 
-                <span><?php echo $nombre['nombre']; ?></span>
+        <?php $proyecto = obtenerNombreProyecto($id_proyecto);
 
-            <?php endforeach; ?>
+            if($proyecto){ ?>
 
-        </h1>
+            <h1>Proyecto Actual:
+                
+                <?php foreach($proyecto as $nombre){ ?>
 
-        <form action="#" class="agregar-tarea">
-            <div class="campo">
-                <label for="tarea">Tarea:</label>
-                <input type="text" placeholder="Nombre Tarea" class="nombre-tarea"> 
-            </div>
-            <div class="campo enviar">
-                <input type="hidden" id="<?php echo $id_proyecto; ?>" value="id_proyecto">
-                <input type="submit" class="boton nueva-tarea" value="Agregar">
-            </div>
-        </form>
+                    <span><?php echo $nombre['nombre']; ?></span>
+
+                <?php } ?>
+
+            </h1>
+
+            <form action="#" class="agregar-tarea">
+                <div class="campo">
+                    <label for="tarea">Tarea:</label>
+                    <input type="text" placeholder="Nombre Tarea" class="nombre-tarea"> 
+                </div>
+                <div class="campo enviar">
+                    <input type="hidden" id="<?php echo $id_proyecto; ?>" value="id_proyecto">
+                    <input type="submit" class="boton nueva-tarea" value="Agregar">
+                </div>
+            </form>
+
+        <?php } else {
+            // Si no hay proyecto seleccionado
+            echo "<p>Selecciona un Proyecto a la izquierda</p>";
+        } ?>
         
- 
 
         <h2>Listado de tareas:</h2>
 
         <div class="listado-pendientes">
             <ul>
-
                 <li id="tarea:<?php echo $tarea['id'] ?>" class="tarea">
                 <p>Cambiar el Logotipo</p>
                     <div class="acciones">
@@ -53,7 +61,9 @@
                 </li>  
             </ul>
         </div>
+
     </main>
+
 </div><!--.contenedor-->
 
 <?php include "includes/templates/footer.php"; ?>
